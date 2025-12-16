65d6b59aa1b6f4e6c434cb41ec6394513551b189
mantree <MantreeDeng@poweronlabs.ai>
2025-12-16 18:49:11 +0800

    增加mysql 配置

--- src/main/java/com/gosh/cons/CommonConstants.java
+++ src/main/java/com/gosh/cons/CommonConstants.java
@@ -2,7 +2,7 @@ package com.gosh.cons;
 
 public final class CommonConstants {
     public static final String FLINK_DEFAULT_CONF = "flink-config.properties";
-    public static final String KAFKA_DEFAULT_CONF = "kakfa-config.properties";
+    public static final String KAFKA_DEFAULT_CONF = "kafka-config.properties";
     public static final String REDIS_DEFAULT_CONF = "redis-config.properties";
     public static final String DEFAULT_REGION = "ap-southeast-1";
     public static final String MYSQL_DEFAULT_CONF = "mysql-config.properties";
