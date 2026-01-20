3418048c4ddef8718e60eed6d8710132eda1b4e1
SamuelXie999 <SamuelXie@poweronlabs.ai>
2026-01-20 10:23:53 +0800

    fresh proto

--- src/main/java/com/gosh/job/ItemFeature48hJob.java
+++ src/main/java/com/gosh/job/ItemFeature48hJob.java
@@ -1,7 +1,7 @@
 package com.gosh.job;
 
 import com.gosh.config.RedisConfig;
-import com.gosh.entity.RecFeature;
+import com.gosh.feature.RecFeature;
 import com.gosh.job.AiTagParseCommon.PostInfoEvent;
 import com.gosh.job.AiTagParseCommon.PostTagsEventParser;
 import com.gosh.job.AiTagParseCommon.PostTagsToPostInfoMapper;
